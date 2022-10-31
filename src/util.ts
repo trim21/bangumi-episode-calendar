2b883fe56b1497909a71d9c61433d50df66e1eb3
Trim21 <trim21.me@gmail.com>
2022-11-01 07:10:10 +0800

    better use chain hash api

--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,5 @@
 import * as crypto from "crypto";
 
 export function uuidByString(v: string): string {
-  const h = crypto.createHash("sha1");
-  h.write(v);
-
-  return h.digest("hex");
+  return crypto.createHash("black2b-128").update(v, "utf-8").digest("hex");
 }
