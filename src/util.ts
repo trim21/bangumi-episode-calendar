178cdbf2baf5364c7830f22e8bae43b2fe3da8ca
Trim21 <trim21.me@gmail.com>
2022-11-01 07:15:26 +0800

    fix hash

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,5 @@
 import * as crypto from "crypto";
 
 export function uuidByString(v: string): string {
-  return crypto.createHash("black2b-128").update(v, "utf-8").digest("hex");
+  return crypto.createHash("SHA-512/256").update(v, "utf-8").digest("hex");
 }
