ed1780c56bb8a0a95edadd1a64518edc76fc8ca2
Trim21 <trim21.me@gmail.com>
2022-11-29 06:47:19 +0800

    avoid setting stdout stream listener limit

--- src/logger.ts
+++ src/logger.ts
@@ -1,18 +1,26 @@
+import * as stream from "stream";
+
 import * as winston from "winston";
 
 export const logger = winston.createLogger({
   level: "info",
 });
 
+const out = new stream.PassThrough();
+
+out.pipe(process.stdout);
+
 if (process.env.NODE_ENV === "production") {
   logger.add(
-    new winston.transports.Console({
+    new winston.transports.Stream({
+      stream: out,
       format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
     }),
   );
 } else {
   logger.add(
-    new winston.transports.Console({
+    new winston.transports.Stream({
+      stream: out,
       format: winston.format.combine(
         winston.format.timestamp(),
         winston.format.colorize({
