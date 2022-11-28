2cb2efa8e67f4a3db3dbe204d76165719c04bb35
Trim21 <trim21.me@gmail.com>
2022-11-29 06:52:23 +0800

    set nestjs logger

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,25 @@ import type { NestFastifyApplication } from "@nestjs/platform-fastify";
 import { AppModule } from "@/module";
 import { logger } from "@/logger";
 
-const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
+  logger: {
+    log(message: any, ...optionalParams): any {
+      logger.info(message, ...optionalParams);
+    },
+    error(message: any, ...optionalParams): any {
+      logger.error(message, ...optionalParams);
+    },
+    debug(message: any, ...optionalParams): any {
+      logger.debug(message, ...optionalParams);
+    },
+    warn(message: any, ...optionalParams): any {
+      logger.warn(message, ...optionalParams);
+    },
+    verbose(message: any, ...optionalParams): any {
+      logger.verbose(message, ...optionalParams);
+    },
+  },
+});
 await app.listen(3000, "0.0.0.0");
 
 logger.info("server started");
