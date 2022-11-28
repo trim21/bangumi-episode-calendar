ae457eca84e4f74fff64cac198600936b05fd160
Trim21 <trim21.me@gmail.com>
2022-11-29 05:21:36 +0800

    use blake 2b

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,7 @@
 import * as crypto from "crypto";
 
 export function uuidByString(v: string): string {
-  const s = crypto.createHash("BLAKE2s256").update(v, "utf-8").digest("hex");
+  const s = crypto.createHash("blake2b512").update(v, "utf-8").digest("hex");
 
   return `${s.slice(0, 8)}-${s.slice(8, 12)}-${s.slice(12, 16)}-${s.slice(16, 20)}-${s.slice(20, 32)}`;
 }
