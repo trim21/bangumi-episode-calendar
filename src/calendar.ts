8e1bcea79b8010c85d7e4ca60ae76c5d894396e6
Trim21 <trim21.me@gmail.com>
2023-02-15 07:04:57 +0800

    refactor: set `REFRESH-INTERVAL` to 8h

--- src/calendar.ts
+++ src/calendar.ts
@@ -190,7 +190,7 @@ function renderICS(subjects: SlimSubject[]): string {
   const calendar = new ICalendar({
     name: "Bangumi Episode Air Calendar",
     "X-PUBLISHED-TTL": "PT8H",
-    "REFRESH-INTERVAL;VALUE=DURATION": "PT24H",
+    "REFRESH-INTERVAL;VALUE=DURATION": "PT8H",
   });
   const today = unix();
 
