0aadb9252c7adfe21d863d71809a907b727227fd
Trim21 <trim21.me@gmail.com>
2024-09-19 01:03:25 +0800

    rename uuid

--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,8 @@
-import { v5 as uuidV5 } from "uuid";
+import { v5 as UUIDv5 } from "uuid";
 
 export function uuidByString(v: string): string {
   // uuid v5 will generate same result for same (name, namespace) pair
-  return uuidV5(v, "ef2256c4-162e-446b-9ccf-81050809d0c9");
+  return UUIDv5(v, "ef2256c4-162e-446b-9ccf-81050809d0c9");
 }
 
 export function notNull<T>(x: T): x is Exclude<T, null> {
